362b5d7b073e6dd1de8f42ec2aa127f4fa4ce417
Chelsea <cjv7mm@hotmail.com>
2026-05-04 10:59:20 -0700

    sitemap added

---
+++ nextjs/app/sitemap.js
@@ -0,0 +1,20 @@
+export default function sitemap() {
+    return [
+        {
+            url: 'portfolio-nextjs-three-wine.vercel.app',
+            lastModified: new Date(),
+        },
+        {
+            url: 'portfolio-nextjs-three-wine.vercel.app/about',
+            lastModified: new Date(),
+        },
+        {
+            url: 'portfolio-nextjs-three-wine.vercel.app/contact',
+            lastModified: new Date(),
+        },
+        {
+            url: 'portfolio-nextjs-three-wine.vercel.app/portfolio',
+            lastModified: new Date(),
+        },
+    ]
+}
