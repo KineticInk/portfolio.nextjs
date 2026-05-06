7683239f1787c3c624b3c7a4b98c9016a86a6bc1
Chelsea <cjv7mm@hotmail.com>
2026-05-06 10:36:06 -0700

    Update layout.tsx

--- nextjs/app/layout.tsx
+++ nextjs/app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -27,7 +28,8 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    >      <GoogleAnalytics gaId="G-TSM6LR7ZXE" />
+
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
